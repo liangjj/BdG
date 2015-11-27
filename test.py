e152533005032784c626e193ccb03652817cc596
Sam Roelants <roelants.sam@gmail.com>
2015-11-27 18:01:06 +0100

    Updated test file to test the new features (integrator)

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
-from parameters import Material, Structure, Parameters
+from parameters import Material, Structure, Parameters, Integrator
 import numpy as np
-from core import Hamiltonian
+from core import Hamiltonian, System
 tin = Material(148.0, 0.000618, 0.666666, 0.1)
 
-struct = Structure(np.array([50]))
+struct = Structure(np.array([50, 30, 20]))
 params = Parameters(tin, struct)
 print(params.print_parameters())
-ham = Hamiltonian(struct)
+ham = Hamiltonian()
+integrator = Integrator(struct)
 
-k = np.array([[1, 2], [3, 4], [5, 6]])
-i = np.array([0, 1])
-
-print(ham.spectrum(k, i))
-print(i.shape)
-print(i.T.shape)
+print(integrator.integrate(ham.spectrum, [10, 10, 10]))
