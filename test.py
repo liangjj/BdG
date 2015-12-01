0ac204476f53e7be5465ef1601ce4d96470b9c31
Sam Roelants <roelants.sam@gmail.com>
2015-12-01 17:15:54 +0100

    Added random testing

--- test.py
+++ test.py
@@ -2,8 +2,9 @@ from parameters import Material, Structure, Parameters, Integrator
 import numpy as np
 from core import Hamiltonian, System
 tin = Material(148.0, 0.000618, 0.666666, 0.1)
+Lz = 50.0
 
-struct = Structure(np.array([50, 30, 20]))
+struct = Structure(np.array([Lz]))
 params = Parameters(tin, struct)
 print(params.print_parameters())
 ham = Hamiltonian()
@@ -12,3 +13,4 @@ sys = System(ham, params, struct)
 
 print(integrator.integrate(ham.spectrum, [10, 10, 10]))
 print(sys.getTc())
+print(struct.cont_dims)
